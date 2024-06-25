1e4c78541ee9e31007803516eb2e64ec80d5c820
June Pratt <juneprattm@gmail.com>
2024-06-25 12:32:33 -0400

    change id to string or number

--- routes/get.ts
+++ routes/get.ts
@@ -1,11 +1,8 @@
 import { RecordId, Surreal } from "surrealdb.js";
 
-//Copilot: make a function that takes a string and gets the record form the surreal database
-//the method to get a record is db.select<recordType>("recordName")
-
 export async function getRecord(
     table: string,
-    id: string | number | bigint | Record<string, unknown> | unknown[],
+    id: string | number,
     db: Surreal
 ) {
     let recordName = new RecordId(table, id);
