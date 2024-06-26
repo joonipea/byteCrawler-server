61bafabb0acc02fcc6811a122a0b6c06e859d60a
June Pratt <juneprattm@gmail.com>
2024-06-26 14:50:28 -0400

    log the record

--- routes/get.ts
+++ routes/get.ts
@@ -2,6 +2,8 @@ import { RecordId, Surreal } from "surrealdb.js";
 
 export async function getRecord(table: string, id: string, db: Surreal) {
     let recordName = new RecordId(table, id);
+    console.log(recordName);
     let record = await db.select(recordName);
+    console.log(record);
     return record;
 }
