7930dd256d3d59094b907c3e3317a6eb4e0d08b2
June Pratt <juneprattm@gmail.com>
2024-06-28 14:15:29 -0400

    don't use numbers in id

--- routes/get.ts
+++ routes/get.ts
@@ -1,6 +1,7 @@
 import { RecordId, Surreal } from "surrealdb.js";
 
 export async function getRecord(table: string, id: any, db: Surreal) {
+    if (table == "maps") id = `floor_${id.toString()}`;
     let recordName = new RecordId(table, id.toString());
     console.log(recordName);
     let record = await db.select(recordName);
