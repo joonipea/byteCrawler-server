766f344d360c9ec0f90061855fb703b4132142be
June Pratt <juneprattm@gmail.com>
2024-06-28 14:40:59 -0400

    use record Id for world

--- generation/generateWorld.ts
+++ generation/generateWorld.ts
@@ -1,14 +1,15 @@
 import { generateChars } from "./generateChars";
 import { generateItems } from "./generateItems";
 import { generateMobs } from "./generateMobs";
-import { Surreal } from "surrealdb.js";
+import { RecordId, Surreal } from "surrealdb.js";
 import { generateMaps } from "./generateMaps";
 import { item } from "../schemas/item";
 
 export async function generateWorld(user: string, db: Surreal) {
-    let [exists] = await db.select(`world:${user}`);
+    const world = new RecordId("world", user);
+    let exists = await db.select(world);
     if (exists) return console.log("World already exists");
-    await db.create(`world:${user}`, { name: user });
+    await db.create(world, { name: user });
     console.log(await generateItems(db, 50));
     console.log(await generateChars(db, 50));
     console.log(await generateMobs(db, 50));
